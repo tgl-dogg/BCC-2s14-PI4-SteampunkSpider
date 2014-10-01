a1712acbafb433fcdfb7b8217a3f90ae939078dc
[TGL] Dogg <vinidoggll@hotmail.com>
2014-10-01 16:24:30 -0300

    Scrapy at glance tutorial

---
+++ src/scrapy_glance/scrapy_glance/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for scrapy_glance project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'scrapy_glance'
+
+SPIDER_MODULES = ['scrapy_glance.spiders']
+NEWSPIDER_MODULE = 'scrapy_glance.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'scrapy_glance (+http://www.yourdomain.com)'
