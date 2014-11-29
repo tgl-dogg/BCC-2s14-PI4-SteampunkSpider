081b5d8e3861d0869201541ca03fa4e0d83e4bac
Danilo Makoto Ikuta <danilo0195@hotmail.com>
2014-11-29 17:24:29 -0200

    Visualizacao lancamentos por estacao ver. final

--- src/steampunk_spider/database/lancamentoEstacao.py
+++ src/steampunk_spider/database/lancamentoEstacao.py
@@ -10,19 +10,34 @@ cursor = db_conn.cursor()
 
 request = ("SELECT "
 	"(SELECT COUNT(software.release_date) FROM software "
-		"WHERE software.release_date LIKE '%Jan%' OR '%Feb%' OR '%Mar%') AS Winter, "
+		"WHERE software.release_date LIKE '%Jan%') AS Jan, "
 	"(SELECT COUNT(software.release_date) FROM software "
-		"WHERE software.release_date LIKE '%Apr%' OR '%May%' OR '%Jun%') AS Spring, "
+		"WHERE software.release_date LIKE '%Feb%') AS Feb, "
 	"(SELECT COUNT(software.release_date) FROM software "
-		"WHERE software.release_date LIKE '%Jul%' OR '%Aug%' OR '%Sep%') AS Summer, "
+		"WHERE software.release_date LIKE '%Mar%') AS Mar, "
 	"(SELECT COUNT(software.release_date) FROM software "
-		"WHERE software.release_date LIKE '%Oct%' OR '%Nov%' OR '%Dec%') AS Fall")
+		"WHERE software.release_date LIKE '%Apr%') AS Apr, "
+	"(SELECT COUNT(software.release_date) FROM software "
+		"WHERE software.release_date LIKE '%May%') AS May, "
+	"(SELECT COUNT(software.release_date) FROM software "
+		"WHERE software.release_date LIKE '%Jun%') AS Jun, "
+	"(SELECT COUNT(software.release_date) FROM software "
+		"WHERE software.release_date LIKE '%Jul%') AS Jul, "
+	"(SELECT COUNT(software.release_date) FROM software "
+		"WHERE software.release_date LIKE '%Aug%') AS Aug, "
+	"(SELECT COUNT(software.release_date) FROM software "
+		"WHERE software.release_date LIKE '%Sep%') AS Sep, "
+	"(SELECT COUNT(software.release_date) FROM software "
+		"WHERE software.release_date LIKE '%Oct%') AS Oct, "
+	"(SELECT COUNT(software.release_date) FROM software "
+		"WHERE software.release_date LIKE '%Nov%') AS Nov, "
+	"(SELECT COUNT(software.release_date) FROM software "
+		"WHERE software.release_date LIKE '%Dec%') AS 'Dec'")
 
 cursor.execute(request)
 
-for Winter, Spring, Summer, Fall in cursor:
-
-    print Winter, Spring, Summer, Fall
+for Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec in cursor:
+	print Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec
 
 cursor.close()
 db_conn.close()
--- src/steampunk_spider/visualisacoes/lancamentosPorEstacao.py
+++ src/steampunk_spider/visualisacoes/lancamentosPorEstacao.py
@@ -16,45 +16,91 @@ endpath(draw = False)
 
 drawpath(c)
         
-def createPath(ia, ea, pathName, tam):
+def createPath(ia, ea):
     stroke(0)
     pi = (768 * cos(ia), 768 * sin(ia))
     pf = (768 * cos(ea), 768 * sin(ea))
-
-    ma = ia + ((ea - ia) / 2)
-    pm = (768 * cos(ma), 768 * sin(ma))
     
-    pathArc = findpath([pi, pm, pf], 1)
     pathCone = findpath([pi, pf, (0, 0)], 0)
     
-    path = c.intersect(pathArc.union(pathCone))
+    path = c.intersect(pathCone)
     drawpath(path)
     
-    push()
+    return pi, pf
+    
+def drawText(name, tam, p):
     fill(0)
-    font("Helvetica", tam / 8)
+    font("Helvetica", tam / 25)
     align(CENTER)
-
-    text(pathName, pm[0] / 2  - textmetrics(pathName, tam / 10)[0], pm[1] / 2 + textmetrics(pathName, tam / 10)[0])
-    pop()
+    text(name, p[0] - textwidth(name) / 2, p[1] + textheight(name) / 8)
 
 def drawGraph(winter, spring, summer, fall):
-    total = float(winter + spring + summer + fall)
-    aWinter = radians(winter / total * 360)
-    aSpring = radians(spring / total * 360) + aWinter
-    aSummer = radians(summer / total * 360) + aSpring
-    
-    angle = 0
+    total = float(sum(winter) + sum(spring) + sum(summer) + sum(fall))
 
-    fill(0.94, .97, 1, 1)    
-    createPath(0, aWinter, "Winter", winter)
-    fill(1, .88, 1, 1)
-    createPath(aWinter, aSpring, "Spring", spring)
-    fill(1, 1, .05, 1)
-    createPath(aSpring, aSummer, "Summer", summer)
-    fill(.87, .46, .28, 1)
-    createPath(aSummer, 2 * pi, "Fall", fall)
+    stack = 0
     
+    i = (0, 0)
+    f = (0, 0)
+    
+    fill(0.94, .97, 1, 1)
+    for month in winter:
+        angle = radians(month / total * 360)
+        pi, pf = createPath(stack, angle + stack)
+        stack += angle
+        
+        if winter.index(month) == 0:
+            i = pi
+        if winter.index(month) == 2:
+            f = pf
+            
+    p = ((i[0] + f[0]) / 4, (i[1] + f[1]) / 4)
+    drawText("Winter", float(sum(winter)), p)
+    
+    
+    fill(1, .88, 1, 1)
+    for month in spring:
+        angle = radians(month / total * 360)
+        pi, pf = createPath(stack, angle + stack)
+        stack += angle
+        
+        if spring.index(month) == 0:
+            i = pi
+        if spring.index(month) == 2:
+            f = pf
+            
+    p = ((i[0] + f[0]) / 4, (i[1] + f[1]) / 4)
+    drawText("Spring", float(sum(spring)), p)
+        
+        
+    fill(1, 1, .05, 1)
+    for month in summer:
+        angle = radians(month / total * 360)
+        pi, pf = createPath(stack, angle + stack)
+        stack += angle
+        
+        if summer.index(month) == 0:
+            i = pi
+        if summer.index(month) == 2:
+            f = pf
+            
+    p = ((i[0] + f[0]) / 4, (i[1] + f[1]) / 4)
+    drawText("Summer", float(sum(summer)), p)
+        
+        
+    fill(.87, .46, .28, 1)
+    for month in fall:
+        angle = radians(month / total * 360)
+        pi, pf = createPath(stack, angle + stack)
+        stack += angle
+        
+        if fall.index(month) == 0:
+            i = pi
+        if fall.index(month) == 2:
+            f = pf
+            
+    p = ((i[0] + f[0]) / 4, (i[1] + f[1]) / 4)
+    drawText("Fall", float(sum(fall)), p)
+        
 DB_NAME = 'steampunk'
 
 # Get database connection
@@ -63,18 +109,38 @@ db_conn = mysql.connector.connect(user='root', password='vaporAranha', host='loc
 cursor = db_conn.cursor()
 
 request = ("SELECT "
-	"(SELECT COUNT(software.release_date) FROM software "
-		"WHERE software.release_date LIKE '%Jan%' OR '%Feb%' OR '%Mar%') AS Winter, "
-	"(SELECT COUNT(software.release_date) FROM software "
-		"WHERE software.release_date LIKE '%Apr%' OR '%May%' OR '%Jun%') AS Spring, "
-	"(SELECT COUNT(software.release_date) FROM software "
-		"WHERE software.release_date LIKE '%Jul%' OR '%Aug%' OR '%Sep%') AS Summer, "
-	"(SELECT COUNT(software.release_date) FROM software "
-		"WHERE software.release_date LIKE '%Oct%' OR '%Nov%' OR '%Dec%') AS Fall")
+    "(SELECT COUNT(software.release_date) FROM software "
+        "WHERE software.release_date LIKE '%Jan%') AS Jan, "
+    "(SELECT COUNT(software.release_date) FROM software "
+        "WHERE software.release_date LIKE '%Feb%') AS Feb, "
+    "(SELECT COUNT(software.release_date) FROM software "
+        "WHERE software.release_date LIKE '%Mar%') AS Mar, "
+    "(SELECT COUNT(software.release_date) FROM software "
+        "WHERE software.release_date LIKE '%Apr%') AS Apr, "
+    "(SELECT COUNT(software.release_date) FROM software "
+        "WHERE software.release_date LIKE '%May%') AS May, "
+    "(SELECT COUNT(software.release_date) FROM software "
+        "WHERE software.release_date LIKE '%Jun%') AS Jun, "
+    "(SELECT COUNT(software.release_date) FROM software "
+        "WHERE software.release_date LIKE '%Jul%') AS Jul, "
+    "(SELECT COUNT(software.release_date) FROM software "
+        "WHERE software.release_date LIKE '%Aug%') AS Aug, "
+    "(SELECT COUNT(software.release_date) FROM software "
+        "WHERE software.release_date LIKE '%Sep%') AS Sep, "
+    "(SELECT COUNT(software.release_date) FROM software "
+        "WHERE software.release_date LIKE '%Oct%') AS Oct, "
+    "(SELECT COUNT(software.release_date) FROM software "
+        "WHERE software.release_date LIKE '%Nov%') AS Nov, "
+    "(SELECT COUNT(software.release_date) FROM software "
+        "WHERE software.release_date LIKE '%Dec%') AS 'Dec'")
 
 cursor.execute(request)
 
-for winter, spring, summer, fall in cursor:
+for Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec in cursor:
+    winter = [Jan, Feb, Mar]
+    spring = [Apr, May, Jun]
+    summer = [Jul, Aug, Sep]
+    fall = [Oct, Nov, Dec]
     drawGraph(winter, spring, summer, fall)
 
 cursor.close()
